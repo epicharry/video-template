550f855eb088b0193b2f7b494ae7cac8a479aca9
Harry <38373650+epicharry@users.noreply.github.com>
2025-11-06 11:16:15 +0530

    Updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'rule34video.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdne-pics.youjizz.com',   // ✅ Add this
+      },
     ],
   },
 };
